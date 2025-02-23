d9bcffbfc9e2a4aa7ada155cd1f86ed68011cf7a
maximum-Destruction <37051448+I-like-pie123@users.noreply.github.com>
2025-02-23 12:33:05 -0500

    Update maze-explorer.py

--- maze-explorer.py
+++ maze-explorer.py
@@ -1,5 +1,6 @@
 import random
 import utils
+import heapq
 
 M = N = 40
 empty_cell = " "
@@ -30,5 +31,45 @@ utils.visualize_maze(maze, M, N, (empty_cell, wall_cell, agent_cell, goal_cell, 
 # while developing the algorithm, you can comment out the visualizer call
 # so that it doesn't open every time you test the code
 
+def astar(maze, start, goal):
+    open_set = []
+    heapq.heappush(open_set, (0, start))
+    came_from = {}
+    g_score = {start: 0}
+    f_score = {start: utils.manhattan_distance(start, goal)}
+
+    while open_set:
+        fscore, current = heapq.heappop(open_set)
+
+        if current == goal:
+            path = []
+            while current in came_from:
+                path.append(current)
+                current = came_from[current]
+            path.append(start)
+            print ("Found a path!")
+            return path[::-1]
+
+        for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            neighbor = (current[0] + dr, current[1] + dc)
+            if 0 <= neighbor[0] < M and 0 <= neighbor[1] < N and maze[neighbor[0]][neighbor[1]] != wall_cell:
+                tentative_g_score = g_score[current] + 1
+                if neighbor not in g_score or tentative_g_score < g_score[neighbor]:
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_g_score
+                    f_score[neighbor] = tentative_g_score + utils.manhattan_distance(neighbor, goal)
+                    heapq.heappush(open_set, (f_score[neighbor], neighbor))
+
+    print ("No path found ):")
+    return None
+path = astar(maze, agent_coords, goal_coords)
+
+if path:
+    for step in path[1:-1]:
+        maze[step[0]][step[1]] = path_cell
+
+
+for row in maze:
+    print(" ".join(row))
 # once we'll be done with actually solving the maze, I can add a visualization for that,
 # showing how the agent moved
